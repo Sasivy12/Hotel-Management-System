3c2f3e2c5ffce44557b0f2da4db55b209a2f3163
Sasivy12 <muslimov.tim05@gmail.com>
2024-04-06 16:19:32 +0300

    added guests.html and getGuests()

--- src/main/java/com/example/hotel/Controller.java
+++ src/main/java/com/example/hotel/Controller.java
@@ -1,16 +1,33 @@
 package com.example.hotel;
 
+import com.example.hotel.Guest.Guest;
+import com.example.hotel.Guest.GuestRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.ui.Model;
 
-@RestController
+import java.util.List;
+
+@org.springframework.stereotype.Controller
 @RequestMapping("/")
 public class Controller
 {
-    @GetMapping("/")
+    @Autowired
+    private GuestRepository guestRepository;
+
+    @GetMapping("/hello")
     public String hello()
     {
         return "Hello World";
+    }
+
+    @GetMapping("/guests")
+    public String getGuests(Model model)
+    {
+        List<Guest> guests = guestRepository.findAll();
+        model.addAttribute("guests", guests);
+
+        return "guests";
     }
 }
